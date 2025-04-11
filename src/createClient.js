e7198122ad149082a23d913b205ec89fe2b1058c
Brainard <brainard.setiawan@gmail.com>
2025-04-12 00:18:10 +0700

    fix gitignore and improvement

--- src/createClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient("https://yfphisweagtquohqwapr.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmcGhpc3dlYWd0cXVvaHF3YXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3OTcxMjQsImV4cCI6MjA0MzM3MzEyNH0.EpHf_WX5OMv2npEFlMWju3LlWviATGlupRFmAgp72Gs")
